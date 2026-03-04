2ce7edf060f5248e29b7611865889f5de4766ef0
Sahan Sasindu <119693671+sahansasindu@users.noreply.github.com>
2026-03-04 13:28:38 +0530

    add

---
+++ src/main/java/com/cicd_sample_project/ci_cd_demo_project/CiCdDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cicd_sample_project.ci_cd_demo_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdDemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cicd_sample_project/ci_cd_demo_project/CiCdDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd_sample_project.ci_cd_demo_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
