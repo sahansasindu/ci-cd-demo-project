78740c247c37271e1a7dba2784fdfd03daafdbd8
Sahan Sasindu <119693671+sahansasindu@users.noreply.github.com>
2026-03-05 16:07:13 +0530

    update

--- src/main/java/com/cicd_sample_project/ci_cd_demo_project/controller/UserController.java
+++ src/main/java/com/cicd_sample_project/ci_cd_demo_project/controller/UserController.java
@@ -23,7 +23,7 @@ public class UserController {
     @PostMapping()
     public ResponseEntity<?> addNewUser(@RequestBody UserReqDto userReqDto) {
         userService.addNewUser(userReqDto);
-        System.out.println("Hellow1111");
+        System.out.println("Hellow11111");
         return ResponseEntity.ok(new CommonResponse<>(true, "User added successfully"));
     }
 
